d3b900e4a7a8724c058dc6896cbce01c3931a38b
shamsutty2419-lab <shamsutty2419@gmail.com>
2026-01-21 15:38:40 +0530

    initiail commit

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -12,10 +12,11 @@ export const appConfig: ApplicationConfig = {
 
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { User } from "./user/user";
 @Component({
   selector: 'app-root',
   standalone: true,
-   imports: [HeaderComponent],
+   imports: [HeaderComponent, User],
   templateUrl: './app.component.html',
   styleUrls:  ['./app.component.css'],
 })
--- my-app/src/app/user/user.ts
+++ my-app/src/app/user/user.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { DUMMY_USERS } from '../dummy-user';
+const randomUserIndex = Math.floor(Math.random() * DUMMY_USERS.length); 
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
+  privateselectedUser = DUMMY_USERS[randomUserIndex];
 
 }
